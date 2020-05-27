974d840eb64efe86f5f2f54b891dc436118dafc8
panwaclaw <vkozlovsk@gmail.com>
2020-05-27 14:29:26 +0300

    going live try #2

--- dolg/settings.py
+++ dolg/settings.py
@@ -32,7 +32,7 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".ngrok.io", ".herokuapp.com"]
 
